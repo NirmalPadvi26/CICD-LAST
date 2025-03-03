3144157a8c6baf2e05741e455521365c1c91b9c2
NirmalPadvi26 <Nirmalp2632@gmail.com>
2025-03-03 14:40:43 +0530

    Fix CodeDeploy overwrite issue

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = 8080;
 const HOST = "0.0.0.0"; // Ensure this is set
 
 app.get("/", (req, res) => {
-  res.send("Hello, Nirmal to the CICD two!");
+  res.send("Hello, Nirmal to the CICD two four!");
 });
 
 app.listen(PORT, HOST, () => {
